bd0105433ec96bfbcf7b9f6a67a672a3c5e33390
garrottkai <garrottkai@gmail.com>
2019-07-30 14:52:02 +0800

    manually recreating files view

---
+++ app/src/main/java/com/kaigarrott/musicplayer/FilesAdapter.kt
@@ -0,0 +1,26 @@
+package com.kaigarrott.musicplayer
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Adapter for files view data
+ */
+class FilesAdapter () : RecyclerView.Adapter<FilesAdapter.Holder> () {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    inner class Holder () : RecyclerView.ViewHolder () {
+
+    }
+}
--- app/src/main/java/com/kaigarrott/musicplayer/FilesFragment.kt
+++ app/src/main/java/com/kaigarrott/musicplayer/FilesFragment.kt
@@ -1,99 +1,42 @@
 package com.kaigarrott.musicplayer
 
-import android.content.Context
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-import com.kaigarrott.musicplayer.dummy.DummyContent
-import com.kaigarrott.musicplayer.dummy.DummyContent.DummyItem
 
 /**
- * A fragment representing a list of Items.
- * Activities containing this fragment MUST implement the
- * [FilesFragment.OnListFragmentInteractionListener] interface.
+ * Displays the file explorer view
+ *
  */
 class FilesFragment : Fragment() {
 
-    // TODO: Customize parameters
-    private var columnCount = 1
+    //private lateinit var view: RecyclerView
+    //private lateinit var adapter: FilesAdapter
+    //private lateinit var manager: RecyclerView.LayoutManager
 
-    private var listener: OnListFragmentInteractionListener? = null
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        //manager = LinearLayoutManager(context)
+        //adapter = FilesAdapter()
 
-        arguments?.let {
-            columnCount = it.getInt(ARG_COLUMN_COUNT)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_files_list, container, false)
-
-        // Set the adapter
+        val view = inflater.inflate(R.layout.fragment_files, container, false)
         if (view is RecyclerView) {
             with(view) {
-                layoutManager = when {
-                    columnCount <= 1 -> LinearLayoutManager(context)
-                    else -> GridLayoutManager(context, columnCount)
-                }
-                adapter = MyFilesRecyclerViewAdapter(DummyContent.ITEMS, listener)
+                setHasFixedSize(true)
+                layoutManager = LinearLayoutManager(context)
+                adapter = FilesAdapter()
             }
         }
         return view
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnListFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException(context.toString() + " must implement OnListFragmentInteractionListener")
-        }
-    }
 
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson
-     * [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
-    interface OnListFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onListFragmentInteraction(item: DummyItem?)
-    }
-
-    companion object {
-
-        // TODO: Customize parameter argument names
-        const val ARG_COLUMN_COUNT = "column-count"
-
-        // TODO: Customize parameter initialization
-        @JvmStatic
-        fun newInstance(columnCount: Int) =
-            FilesFragment().apply {
-                arguments = Bundle().apply {
-                    putInt(ARG_COLUMN_COUNT, columnCount)
-                }
-            }
-    }
 }
