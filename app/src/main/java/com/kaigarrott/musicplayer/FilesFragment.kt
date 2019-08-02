aac3be82d25466074589b15106e3326fda4412b6
garrottkai <garrottkai@gmail.com>
2019-08-02 18:29:23 +0800

    setting up file explorer

--- app/src/main/java/com/kaigarrott/musicplayer/FilesFragment.kt
+++ app/src/main/java/com/kaigarrott/musicplayer/FilesFragment.kt
@@ -3,6 +3,7 @@ package com.kaigarrott.musicplayer
 
 import android.content.Context
 import android.os.Bundle
+import android.os.Environment
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,7 @@ import android.view.ViewGroup
 import android.view.ViewManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.io.File
 
 
 /**
@@ -18,28 +20,32 @@ import androidx.recyclerview.widget.RecyclerView
  */
 class FilesFragment : Fragment() {
 
-    //private lateinit var view: RecyclerView
-    //private lateinit var adapter: FilesAdapter
-    //private lateinit var manager: RecyclerView.LayoutManager
+    private val canRead = Environment.getExternalStorageState() in setOf(Environment.MEDIA_MOUNTED, Environment.MEDIA_MOUNTED_READ_ONLY)
+    private var adapter: FilesAdapter? = null
     private var listener: FilesItemListener? = null
+    private var currentDir: File? = null
 
     override fun onAttach(context: Context?) {
         super.onAttach(context)
         if(context is FilesItemListener) listener = context
+        if(canRead) {
+            currentDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC)
+            adapter = FilesAdapter(listFiles(currentDir), listener)
+
+        } else {
+            // TODO show alert re: storage access
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
-        //manager = LinearLayoutManager(context)
-        //adapter = FilesAdapter()
 
         val view = inflater.inflate(R.layout.fragment_files, container, false)
         if (view is RecyclerView) {
             with(view) {
                 setHasFixedSize(true)
                 layoutManager = LinearLayoutManager(context)
-                adapter = FilesAdapter(listOf(FilesItem("these"), FilesItem("are"), FilesItem("some"), FilesItem("fake"), FilesItem("items")), listener)
             }
+            view.adapter = adapter
         }
         return view
     }
@@ -48,6 +54,8 @@ class FilesFragment : Fragment() {
         super.onDetach()
         listener = null
     }
+
+    fun listFiles(dir: File?): List<FilesItem> = dir.listFiles().map {file -> FilesItem(file.name)}
 
     interface FilesItemListener {
         fun onItemSelected(item: FilesItem?)
